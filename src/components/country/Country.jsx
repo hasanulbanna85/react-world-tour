6b8677fe37aab9543e9dc3fe818252d3963a3ab3
hasanulbanna85 <hasanulbanna265@gmail.com>
2025-03-06 01:59:43 +0600

    mark visited complete

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './Country.css'
+const Country = ({country, handleVisitedCountry}) => {
+    const {name, flags, population, area, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited);
+    }
+
+    return (
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{color: visited ? 'purple' : 'black'}}>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark visited</button>
+            <br/> <br/>
+            <button onClick={handleVisited}>Visited</button>
+            {visited? 'I have visited' : 'I want to visit'}
+        </div>
+    );
+};
+
+export default Country;
